c816ddf4f72b3de5d958bbdfbf628af5518e8c8c
Pavel Khralovich <pkhralovich@gmail.com>
2021-02-24 15:35:13 +0100

    App icon & splash screen

---
+++ app/src/main/java/com/pavel/voicedo/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.pavel.voicedo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
