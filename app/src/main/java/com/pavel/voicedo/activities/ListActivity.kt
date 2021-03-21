e0d26f450ce2feb4ac256c5bf6998dec9b1c6878
Pavel Khralovich <pkhralovich@gmail.com>
2021-03-21 14:45:14 +0100

    Implement UI ListActivity & Force portrait layout

--- app/src/main/java/com/pavel/voicedo/activities/ListActivity.kt
+++ app/src/main/java/com/pavel/voicedo/activities/ListActivity.kt
@@ -2,9 +2,25 @@ package com.pavel.voicedo.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import butterknife.ButterKnife
+import com.pavel.voicedo.R
+import com.pavel.voicedo.models.ShoppingList
 
 class ListActivity : AppCompatActivity() {
+    lateinit var list : ShoppingList
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.list_activity)
+        ButterKnife.bind(this)
+
+        list = getIntent().getSerializableExtra(MainActivity.PARAMS.LIST) as ShoppingList
+
+        updateUI()
+    }
+
+    fun updateUI() {
+        //TODO: Load task data
     }
 }
