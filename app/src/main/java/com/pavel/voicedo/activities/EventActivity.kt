3f2685af58cfa63627c7b42febf86d68cca79855
Pavel Khralovich <pkhralovich@gmail.com>
2021-03-21 14:10:36 +0100

    Implement UI EventActivity

--- app/src/main/java/com/pavel/voicedo/activities/EventActivity.kt
+++ app/src/main/java/com/pavel/voicedo/activities/EventActivity.kt
@@ -2,9 +2,25 @@ package com.pavel.voicedo.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import butterknife.ButterKnife
+import com.pavel.voicedo.R
+import com.pavel.voicedo.models.Event
 
 class EventActivity : AppCompatActivity() {
+    lateinit var event : Event
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.event_activity)
+        ButterKnife.bind(this)
+
+        event = getIntent().getSerializableExtra(MainActivity.PARAMS.EVENT) as Event
+
+        updateUI()
+    }
+
+    fun updateUI() {
+        //TODO: Load task data
     }
 }
